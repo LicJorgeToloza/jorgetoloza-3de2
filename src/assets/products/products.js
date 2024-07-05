199ec5f8b66968c0a23969c33a643295dffc414c
licjorgetoloza <jorgetoloza.dg.ok@gmail.com>
2024-07-04 23:23:17 -0300

    PreEntrega2 TolozaJorge

---
+++ src/assets/products/products.js
@@ -0,0 +1,218 @@
+export let botinesFutbol [
+  {
+      id: 1,
+      title: "Producto 1";
+      description: "Descripción del producto 1";
+      ground: "F5";
+      category: "adidas";
+      price: 29.99;
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275791/5adidas03-b_f11w9y.jpg"
+  },
+  {
+      id: 2,
+      title: "Producto 2",
+      description: "Descripción del producto 2",
+      ground: "F5",
+      category: "adidas",
+      price: 39.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275790/5adidas02-c_pniirn.jpg"
+  },
+  {
+      id: 3,
+      title: "Producto 3",
+      description: "Descripción del producto 3",
+      ground: "F5",
+      category: "adidas",
+      price: 49.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275790/5adidas03-a_vhvr70.jpg"
+  },
+  {
+      id: 4,
+      title: "Producto 4",
+      description: "Descripción del producto 4",
+      ground: "F5",
+      category: "adidas",
+      price: 59.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275790/5adidas02-b_zgtpca.jpg"
+  },
+  {
+      id: 5,
+      title: "Producto 5",
+      description: "Descripción del producto 5",
+      ground: "F5",
+      category: "puma",
+      price: 34.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275791/5puma01-a_zd43h5.jpg"
+  },
+  {
+      id: 6,
+      title: "Producto 6",
+      description: "Descripción del producto 6",
+      ground: "F5",
+      category: "puma",
+      price: 44.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275792/5puma02-a_r13ba3.jpg"
+  },
+  {
+      id: 7,
+      title: "Producto 7",
+      description: "Descripción del producto 7",
+      ground: "F5",
+      category: "puma",
+      price: 54.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275792/5puma03-a_tk9hfo.jpg"
+  },
+  {
+      id: 8,
+      title: "Producto 8",
+      description: "Descripción del producto 8",
+      ground: "F5",
+      category: "puma",
+      price: 64.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275792/5puma03-b_gdwf5f.jpg"
+  },
+  {
+      id: 9,
+      title: "Producto 9",
+      description: "Descripción del producto 9",
+      ground: "F5",
+      category: "nike",
+      price: 39.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275791/5nike05-b_gx8dwu.jpg"
+  },
+  {
+      id: 10,
+      title: "Producto 10",
+      description: "Descripción del producto 10",
+      ground: "F5",
+      category: "nike",
+      price: 49.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275791/5nike01-a_kk3utw.jpg"
+  },
+  {
+      id: 11,
+      title: "Producto 11",
+      description: "Descripción del producto 11",
+      ground: "F5",
+      category: "nike",
+      price: 59.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275791/5nike04-c_ynpe92.jpg"
+  },
+  {
+      id: 12,
+      title: "Producto 12",
+      description: "Descripción del producto 12",
+      ground: "F5",
+      category: "nike",
+      price: 69.99,
+      image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275790/5nike03-c_uesd0x.jpg"
+  },
+  {
+    id: 13,
+    title: "Producto 13";
+    description: "Descripción del producto 13";
+    ground: "F11";
+    category: "adidas";
+    price: 29.99;
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275859/adidas04-b_vbqrs8.jpg"
+},
+{
+    id: 14,
+    title: "Producto 14",
+    description: "Descripción del producto 14",
+    ground: "F11",
+    category: "adidas",
+    price: 39.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275856/adidas04-a_awd7h6.jpg"
+},
+{
+    id: 15,
+    title: "Producto 15",
+    description: "Descripción del producto 15",
+    ground: "F11",
+    category: "adidas",
+    price: 49.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275856/adidas02-a_pu13zy.jpg"
+},
+{
+    id: 16,
+    title: "Producto 16",
+    description: "Descripción del producto 16",
+    ground: "F11",
+    category: "adidas",
+    price: 59.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275856/adidas03-a_jafmzf.jpg"
+},
+{
+    id: 17,
+    title: "Producto 17",
+    description: "Descripción del producto 17",
+    ground: "F11",
+    category: "puma",
+    price: 34.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275871/puma03-a_aryisr.jpg"
+},
+{
+    id: 18,
+    title: "Producto 18",
+    description: "Descripción del producto 18",
+    ground: "F11",
+    category: "puma",
+    price: 44.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275871/puma02-b_vljiue.jpg"
+},
+{
+    id: 19,
+    title: "Producto 19",
+    description: "Descripción del producto 19",
+    ground: "F11",
+    category: "puma",
+    price: 54.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275870/puma02-a_kg6mq6.jpg"
+},
+{
+    id: 20,
+    title: "Producto 20",
+    description: "Descripción del producto 20",
+    ground: "F11",
+    category: "puma",
+    price: 64.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275868/puma01-b_nsuesz.jpg"
+},
+{
+    id: 21,
+    title: "Producto 21",
+    description: "Descripción del producto 21",
+    ground: "F11",
+    category: "nike",
+    price: 39.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275865/nike04-c_xuwuxl.jpg"
+},
+{
+    id: 22,
+    title: "Producto 22",
+    description: "Descripción del producto 22",
+    ground: "F11",
+    category: "nike",
+    price: 49.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275866/nike05-a_fgumgg.jpg"
+},
+{
+    id: 23,
+    title: "Producto 23",
+    description: "Descripción del producto 23",
+    ground: "F11",
+    category: "nike",
+    price: 59.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275862/nike03-a_gav7ae.jpg"
+},
+{
+    id: 24,
+    title: "Producto 24",
+    description: "Descripción del producto 24",
+    ground: "F11",
+    category: "nike",
+    price: 69.99,
+    image_url: "https://res.cloudinary.com/dd7mfvnx8/image/upload/v1719275860/nike02-a_bperbm.jpg"
+},
+];
